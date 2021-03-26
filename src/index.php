a53b650d179ef78f718f3211954ad6b87c7a9f34
Julien Grenier <grenier.julien5@gmail.com>
2021-03-26 15:40:11 +0100

    voila

--- src/index.php
+++ src/index.php
@@ -6,6 +6,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style.css">
     <title>Animaux</title>
 </head>
 <body>
@@ -24,12 +25,12 @@
                     <option value=<?=$value->id?>><?=$value->type?></option>
                 <?php endforeach;?>
             </select>
-            <button type="submit">Soumettre</button>
+            <button class="du-css-pour-cyriak" type="submit">Soumettre</button>
         </form>
 
         <form action="" method="POST">
             <input type="text" name="newRace" placeholder="Ajouter nouvelle race..." required>
-            <button type="sumbit">Soumettre</button>
+            <button class="du-css-pour-cyriak" type="sumbit">Soumettre</button>
         </form>
     </ul>
 </body>
